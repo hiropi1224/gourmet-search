a34277816d5470e5aa5ffd71cc8481b7ae32a564
Shirahamah <hiropi1224s@gmail.com>
2023-01-22 21:46:36 +0900

    👔 APIコール処理修正

--- src/pages/api/search/index.ts
+++ src/pages/api/search/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 // Y035: 原宿・青山・表参道, Y030: 渋谷
-const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.NEXT_PUBLIC_API_KEY}&format=json&middle_area=Y035&middle_area=Y030`;
+const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.NEXT_PUBLIC_API_KEY}&format=json&middle_area=Y035&count=100`;
 
 export default async (req: any, res: any) => {
   let url = defaultEndpoint;
@@ -10,7 +10,16 @@ export default async (req: any, res: any) => {
     url = `${url}&keyword=${req.query.keyword}`;
   }
 
+  if (typeof req.query.isLunch !== undefined) {
+    url = `${url}&keyword=${req.query.keyword}&lunch=${req.query.lunch}`;
+  }
+
+  if (typeof req.query.order !== undefined) {
+    url = `${url}&keyword=${req.query.keyword}&lunch=${req.query.lunch}&order=${req.query.order}`;
+  }
+
   url = encodeURI(url);
+  console.log(url, 'req url');
   const response = await fetch(url);
   const data = await response.json();
   res.json(data);
