11b0cbc0da1b76ef0e0f2727f887f322081b825c
Shirahamah <hiropi1224s@gmail.com>
2023-01-30 23:20:26 +0900

    👔 StoreにPost情報追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,28 @@
 import { Session } from '@supabase/supabase-js';
 import create from 'zustand';
+import { EditedPost } from '@/types';
 
 type State = {
   session: Session | null;
   setSession: (payload: Session | null) => void;
+  editedPost: EditedPost;
+  updateEditedPost: (payload: EditedPost) => void;
+  resetEditedPost: () => void;
 };
 const useStore = create<State>((set) => ({
   session: null,
   setSession: (payload) => set({ session: payload }),
+  editedPost: { id: '', title: '', post_url: '' },
+  updateEditedPost: (payload) =>
+    set({
+      editedPost: {
+        id: payload.id,
+        title: payload.title,
+        post_url: payload.post_url,
+      },
+    }),
+  resetEditedPost: () =>
+    set({ editedPost: { id: '', title: '', post_url: '' } }),
 }));
 
 export default useStore;
