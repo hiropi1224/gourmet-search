d34b649e9d6f9e0188015fd7db6a43551ba57a47
Shirahamah <hiropi1224s@gmail.com>
2023-03-01 21:44:01 +0900

    📦 add storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+// .storybook/preview.tsx
+
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useDarkMode } from 'storybook-dark-mode';
+import React from 'react';
+// import theme object you've exported in previous step
+import { theme } from './theme';
+
+// Create a wrapper component that will contain all your providers.
+// Usually you should render all providers in this component:
+// MantineProvider, NotificationsProvider, ModalsProvider, SpotlightProvider, etc.
+function ThemeWrapper(props: { children: React.ReactNode }) {
+  const colorScheme = useDarkMode() ? 'dark' : 'light';
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={() => {}}>
+      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+        {props.children}
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
+
+// enhance your stories with decorator that uses ThemeWrapper
+export const decorators = [
+  (renderStory: Function) => <ThemeWrapper>{renderStory()}</ThemeWrapper>,
+];
---
+++ src/stories/MantineButton.tsx
@@ -0,0 +1,6 @@
+import { Button } from '@mantine/core';
+import React from 'react';
+
+export const MantineButton = () => {
+  return <Button>MantineButton</Button>;
+};
