46ec522b034ad57ffde129708aa222ba90237d83
Shirahamah <hiropi1224s@gmail.com>
2023-03-02 23:23:10 +0900

    💄 AspectImgコンポーネント作成

---
+++ src/components/features/PostList/AspectImg/AspectImg.tsx
@@ -0,0 +1,34 @@
+import { AspectRatio, Image } from '@mantine/core';
+import React, { FC } from 'react';
+
+type Props = {
+  imgUrl?: string;
+};
+
+export const AspectImg: FC<Props> = ({ imgUrl }) => {
+  if (imgUrl) {
+    return (
+      <AspectRatio
+        ratio={1 / 1}
+        style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}
+      >
+        <Image src={imgUrl} alt='Norway' />
+      </AspectRatio>
+    );
+  } else {
+    return (
+      <AspectRatio
+        ratio={1 / 1}
+        style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}
+      >
+        <Image
+          src='undefined'
+          alt='With default placeholder'
+          width={500}
+          height={500}
+          withPlaceholder
+        />
+      </AspectRatio>
+    );
+  }
+};
---
+++ src/components/features/PostList/AspectImg/index.tsx
@@ -0,0 +1,1 @@
+export { AspectImg } from './AspectImg';
