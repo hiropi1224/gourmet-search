6ce54feaf04dd17a3f28798300b60540d2606e04
Shirahamah <hiropi1224s@gmail.com>
2023-03-21 20:29:39 +0900

    🔧 gaの設定追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import router from 'next/router';
+import { useEffect } from 'react';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import * as gtag from '@/libs/analytics/gtag';
 import useStore from '@/store';
 
 const queryClient = new QueryClient({
@@ -16,6 +19,17 @@ const queryClient = new QueryClient({
 });
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const isDark = useStore((state) => state.isDark);
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <QueryClientProvider client={queryClient}>
